63f136030ff8c40f868fcffa44224cca441100dd
darek.sowada <darek.sowada@localhost>
2020-04-22 21:52:29 +0200

    Apartment model

---
+++ src/apartments/Apartment.java
@@ -0,0 +1,14 @@
+package apartments;
+
+import javax.persistence.Entity;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Apartment {
+  
+	int apartmentNumber;
+	int numberOfRooms;
+	int numberOfBeds;
+}
