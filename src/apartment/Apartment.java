c6ea739740feab62bd0de35c0294b580e7adf961
darek.sowada <darek.sowada@localhost>
2020-04-26 19:50:29 +0200

    test

--- src/apartment/Apartment.java
+++ src/apartment/Apartment.java
@@ -1,14 +1,28 @@
 package apartment;
 
-import javax.persistence.Entity;
+import java.util.UUID;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import hotel.Hotel;
 import lombok.Data;
 
 @Data
 @Entity
 public class Apartment {
   
+	@Id
+	UUID apartmentId;
+	
 	int apartmentNumber;
 	int numberOfRooms;
 	int numberOfBeds;
+	boolean hasBathroom;
+	
+	@ManyToOne(optional=false)
+	@JoinColumn(name="hotel_id", nullable=false)
+	Hotel hotel;
 }
--- src/hotel/Hotel.java
+++ src/hotel/Hotel.java
@@ -1,5 +1,18 @@
 package hotel;
 
+import java.util.Set;
+import java.util.UUID;
+
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import apartment.Apartment;
+
 public class Hotel {
 
+	@Id
+	UUID hotelId;
+	
+	@OneToMany
+	Set<Apartment> apartments;
 }
